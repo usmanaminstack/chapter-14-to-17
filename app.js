9f1613b187474a04db9546c89fc2a13f0a2b4410
AbdulAhad <35425896+abdulahad96@users.noreply.github.com>
2021-04-29 05:50:55 +0500

    committed

---
+++ app.js
@@ -0,0 +1,37 @@
+var arr = [""]
+var arr = {}
+var arr = {"this is string arr"}
+var arr = {}
+var arr = {}
+
+
+
+// var arr = ["ghous"];
+
+// arr[1] = "ahmed";
+
+// arr.push("ahmed","nadir")
+
+// arr.pop()
+// arr.pop()
+// arr.pop()
+
+
+// arr.unshift("ahmed","nadir")
+
+// arr.shift()
+
+// console.log(arr)
+
+// cities.splice(1,0,'peshawar')
+
+ 
+// var cities = ['karachi','lahore','islamabad','quetta'];
+// var name = prompt("Enter city name")
+// var name2 = prompt("Enter city name2")
+
+// cities.push(name,name2)
+
+// console.log(cities)
+
+
